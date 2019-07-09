047d59aeba33f8f01e6497dba07162c3319ea8ab
CharlesFrohlichlab <zhouz@email.unc.edu>
2019-07-09 17:09:27 -0400

    added form to both parallax and app

--- app.py
+++ app.py
@@ -6,6 +6,12 @@ app = Flask(__name__)
 def index():
   return render_template('parallax.html')
 
+@app.route('/', methods=['POST'])
+def my_form_post():
+    text = request.form['text']
+    processed_text = text.upper()
+    return processed_text
+
 @app.route('/about')
 def about():
   return render_template('about.html')
