df615d5fdb4eb2079213ba60a8731b0ff5ccfa7e
CharlesFrohlichlab <zhouz@email.unc.edu>
2019-07-17 20:56:32 -0400

    Revert "test base website"
    This reverts commit 8fae57ac73c772a1e97e3969ee1dcc9d704bfa29.

--- app.py
+++ app.py
@@ -2,10 +2,13 @@ from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def index():
-
-    return render_template("parallax.html")
+    sumNameText = request.form['summonerName']
+    champNameText = request.form['champName']
+    processed_sumName = sumNameText.upper()
+    processed_champName = champNameText.capitalize()
+    return render_template("parallax.html",result = processed_champName)
 
 @app.route('/about')
 def about():
